785167ff14d93e3742d9e91e9aa0f969755b35c0
Parthahere001 <parthahere001@gmail.com>
2022-05-14 13:39:47 +0530

    Added An App called Myapp and added a view

--- project1/myworld/myworld/urls.py
+++ project1/myworld/myworld/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.http import HttpResponse
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+             path('myapp/',
+include('myapp.urls')),
+             path('admin',admin.site.urls),
 ]
