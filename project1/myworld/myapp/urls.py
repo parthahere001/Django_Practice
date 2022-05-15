dce2b0d8a385624a2c9171ae964dcd1e0f09afed
Parthahere001 <parthahere001@gmail.com>
2022-05-15 19:25:30 +0530

    Made a sample HTML page to be previewed using templates

---
+++ project1/myworld/myapp/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from django.http import HttpResponse
+from .import views
+urlpatterns = [path('',views.index,name='index'),]
--- project1/myworld/myworld/settings.py
+++ project1/myworld/myworld/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'myworld.wsgi.application'
 
 
