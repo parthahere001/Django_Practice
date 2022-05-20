2eb24fd14a2c68485ee72bc1f884211fd43890e8
Parthahere001 <parthahere001@gmail.com>
2022-05-20 17:39:17 +0530

    Made an option to update the data (by clicking on the serial number)

--- project1/myworld/myapp/views.py
+++ project1/myworld/myapp/views.py
@@ -24,4 +24,19 @@ def delete(request, id):
   member = Myapp.objects.get(id=id)
   member.delete()
   return HttpResponseRedirect(reverse('index'))
+def update(request, id):
+  mymember = Myapp.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+def updaterecord(request, id):
+  first = request.POST['first']
+  last = request.POST['last']
+  member = Myapp.objects.get(id=id)
+  member.firstname = first
+  member.lastname = last
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
 # Create your views here.
